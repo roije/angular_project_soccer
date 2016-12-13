95d9bdba08f9211c899e74b511ec3a697c0df429
christianhasselstrom <crellecrelle@gmail.com>
2016-12-13 12:45:43 +0100

    create player works

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -3,14 +3,15 @@
  */
 import {Injectable} from "@angular/core";
 import {Player} from "../Entity/player.entity";
-import {Http, Response} from "@angular/http";
+import {Http, Response, RequestOptions, Headers} from "@angular/http";
 import {Observable} from "rxjs";
+import {create} from "domain";
 
 @Injectable()
 export class PlayerService{
 
   private players: Player[];
-  private url: string = "http://soccerapi.herokuapp.com/api/players";
+  private url: string = "http://localhost:3000/players";
 
   constructor(private http : Http){
 
@@ -29,9 +30,36 @@ export class PlayerService{
       })
     .catch(this.handleError);
   }
+/*
+  public getDetailedPlayer(id: string) : Player {
+    let player = this.players.find(player => player._id === id);
+    if(player) {
+
+    }
+
+    return <Player> {player : {} };
+  }
+*/
+
+  public createPlayer(player): Observable<Player> {
+    let options = this.getOptionsObject();
+    return this.http.post(this.url, player, options)
+      .map((res: Response) => {
+      let createdPlayer = res.json();
+      this.players.push(createdPlayer);
+    })
+      .catch(this.handleError);
+
+  }
 
   public handleError(error: Response | any) {
     return Observable.throw("Problem with request");
   }
+
+  private getOptionsObject(): RequestOptions {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return new RequestOptions({ headers: headers });
+  }
+
 
 }
--- src/app/soccer-components/create-player/create-player.component.ts
+++ src/app/soccer-components/create-player/create-player.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
 import {CreatePlayerValidators} from "./create-player.validators";
+import {PlayerService} from "../../services/player.service";
 
 @Component({
   selector: 'app-create-player',
@@ -12,7 +13,7 @@ export class CreatePlayerComponent implements OnInit {
 
   createPlayerForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private route: ActivatedRoute) { }
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private playerservice : PlayerService) { }
 
   ngOnInit() {
     this.createPlayerForm = this.fb.group({
@@ -35,6 +36,13 @@ export class CreatePlayerComponent implements OnInit {
   onSubmit(createPlayerForm) {
     if(createPlayerForm.valid) {
       console.log("valid")
+      var player = {
+        name : createPlayerForm.controls.playername.value,
+        team : createPlayerForm.controls.playerteam.value,
+        position : createPlayerForm.controls.playerposition.value,
+        picture : createPlayerForm.controls.playerpicture.value
+      };
+      this.playerservice.createPlayer(player).subscribe();
     }
     else{
       console.log("invalid")
